74ce425429f250b0c94dced22cee21617b149049
Tom <fougeretom@gmail.com>
2021-03-05 11:49:06 +0100

    Add visualization functions (and their UT)

--- test_visualization.py
+++ test_visualization.py
@@ -1,9 +1,15 @@
-import os
+import tensorflow as tf
+
 from visualization import *
+from ae_models import fcn8
+from processing_functions import preprocessing_images
+from manage_dataset import get_data
 
 IMG_ID = 1
 IMAGE_FOLDER = 'dataset/synthetic_sugarbeet_random_weeds/rgb/'
 MASK_FOLDER = 'dataset/synthetic_sugarbeet_random_weeds/gt/'
+HEIGHT = 256
+WIDTH = 256
 
 
 def test_plot_masks():
@@ -19,3 +25,22 @@ def test_plot_image_with_mask():
     example_mask = cv2.imread(MASK_FOLDER + list_files[IMG_ID])
 
     plot_image_with_mask(example_image, example_mask)
+
+
+def test_plot_prediction():
+
+    model = fcn8(HEIGHT, WIDTH, 3, 1, n_filters=32)
+    model.load_weights('fcn8_weights.h5')
+    example_image, example_mask = get_data('dataset/synthetic_sugarbeet_random_weeds/train_test/test/images/img/',
+                                           'dataset/synthetic_sugarbeet_random_weeds/train_test/test/masks/img/',
+                                           id=0)
+
+    # Preprocessing image to get the right dim
+    image_processed = preprocessing_images(example_image, new_size=(HEIGHT, WIDTH))
+    image_tensor = tf.expand_dims(image_processed, axis=0)
+
+    # Predict output with this image
+    prediction = model.predict(image_tensor)
+    pred_image = np.array(tf.squeeze(prediction, axis=0))
+
+    plot_prediction(example_image, pred_image, example_mask)
--- visualization.py
+++ visualization.py
@@ -1,11 +1,20 @@
 import numpy as np
 import cv2
 import os
+import matplotlib.pyplot as plt
 
-from processing_functions import postprocessing_masks_rgb
+from processing_functions import postprocessing_masks_rgb, postprocessing_masks_prediction, postprocessing_images
 
 
 def plot_masks(mask_folder_path, nb_masks=3, scale_percent=0.6):
+    """
+    Display layers of several masks
+
+    Args:
+      mask_folder_path (string) - ground truth label maps
+      nb_masks (int) - Number of masks to display
+      scale_percent (float) - Scale of image to display
+    """
 
     list_masks = os.listdir(mask_folder_path)
 
@@ -32,8 +41,14 @@ def plot_masks(mask_folder_path, nb_masks=3, scale_percent=0.6):
     cv2.waitKey()
 
 
-# Function to plot an image with its mask
 def plot_image_with_mask(image, mask):
+    """
+    Display an image with its mask (and both)
+
+    Args:
+      image (cv2.Mat) - Image
+      mask (cv2.Mat) - Mask
+    """
     mask_norm = postprocessing_masks_rgb(mask, image.shape)
 
     mixed_image = cv2.addWeighted(image, 0.5, mask_norm, 0.5, 0)
@@ -43,3 +58,67 @@ def plot_image_with_mask(image, mask):
     # Plot image
     cv2.imshow('Left:Image, center:Mask, right:Superposition', example)
     cv2.waitKey()
+
+
+def plot_prediction(image, prediction, gt):
+    """
+    Display an image, the expected output and the prediction
+
+    Args:
+      image (cv2.Mat) - Image
+      prediction (cv2.Mat) - Predicted output (mask)
+      gt (cv2.Mat) - Expected output (mask)
+    """
+
+    image_example_gt = np.concatenate((postprocessing_images(image,
+                                                             new_size=(prediction.shape[0],
+                                                                       prediction.shape[1])),
+                                       postprocessing_masks_rgb(gt,
+                                                                new_size=(prediction.shape[0],
+                                                                          prediction.shape[1])),
+                                       postprocessing_masks_prediction(prediction,
+                                                                       new_size=(prediction.shape[0],
+                                                                                 prediction.shape[1]))),
+                                      axis=1)
+    cv2.imshow('Left:Input, center:Ground Truth, right:Prediction', image_example_gt)
+    cv2.waitKey(0)
+
+
+def plot_loss_accuracy(model_history):
+    """
+    Plot Loss and Accuracy (for training epoch)
+
+    Args:
+      model_history (cv2.Mat) - Image
+      prediction (cv2.Mat) - Predicted output (mask)
+      gt (cv2.Mat) - Expected output (mask)
+    """
+
+    # -----------------------------------------------------------
+    # Retrieve a list of list results on training and test data
+    # sets for each training epoch
+    # -----------------------------------------------------------
+    acc = model_history.history['accuracy']
+    val_acc = model_history.history['val_accuracy']
+    loss = model_history.history['loss']
+    val_loss = model_history.history['val_loss']
+
+    epochs = range(len(acc))  # Get number of epochs
+
+    # ------------------------------------------------
+    # Plot training and validation accuracy per epoch
+    # ------------------------------------------------
+    plt.figure()
+    plt.plot(epochs, acc, 'r')
+    plt.plot(epochs, val_acc, 'b')
+    plt.legend(["Training", "Validation"])
+    plt.title('Accuracy')
+
+    # ------------------------------------------------
+    # Plot training and validation loss per epoch
+    # ------------------------------------------------
+    plt.figure()
+    plt.plot(epochs, loss, 'r')
+    plt.plot(epochs, val_loss, 'b')
+    plt.legend(["Training", "Validation"])
+    plt.title('Loss')
