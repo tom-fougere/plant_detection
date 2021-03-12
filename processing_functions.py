8bb23079094bd0328a6d976bddf8cb4563db70a4
Tom <fougeretom@gmail.com>
2021-03-12 14:10:20 +0100

    Change and improve visualization functions
    Modify UT as well

--- processing_functions.py
+++ processing_functions.py
@@ -10,32 +10,15 @@ def preprocessing_masks(mask):
     return img
 
 
-def preprocessing_images(image, new_size):
-    img = image.astype(np.float32)
-    img = cv2.resize(image, (new_size[1], new_size[0]))
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    img = img / 255.
+def convert_1channel_to_3channels(image):
+    return np.concatenate((image,)*3, axis=-1)
+
+
+def convert_rgb_mask_to_1channel_mask(image):
+    img = image[:, :, 0:1]
     return img
 
 
-# Post processing of images and masks for visualization
-def postprocessing_masks_rgb(mask, new_size):
-    img = mask>0
-    img = img * 255
-    img = img.astype(np.uint8)
-    img = cv2.resize(img, (new_size[1], new_size[0]))
-    return img
-
-
-def postprocessing_masks_prediction(mask, new_size):
-    img = np.zeros(shape=(mask.shape[0], mask.shape[1], 3))
-    img[:, :, 0] = img[:, :, 1] = img[:, :, 2] = mask[:, :, 0]
-    img = img * 255
-    img = img.astype(np.uint8)
-    img = cv2.resize(img, (new_size[1], new_size[0]))
-    return img
-
-
-def postprocessing_images(image, new_size):
-    img = cv2.resize(image, (new_size[1], new_size[0]))
+def convert_rgb_mask_for_displaying(rgb_mask, max_value=1.):
+    img = (rgb_mask * (255./max_value)).astype(np.uint8)
     return img
--- visualization.py
+++ visualization.py
@@ -1,160 +1,150 @@
 import numpy as np
-import cv2
+from cv2 import imread, resize, imshow, waitKey
 import os
-import matplotlib.pyplot as plt
+import random
 
-from processing_functions import postprocessing_masks_rgb, postprocessing_masks_prediction, postprocessing_images
+from processing_functions import convert_1channel_to_3channels, convert_rgb_mask_for_displaying
 
 
-def plot_masks(mask_folder_path, nb_masks=3, scale_percent=0.6):
+def merge_images(images: list, axis=0):
     """
-    Display layers of several masks
+    Merge list of images
+    :param images: List of images, np-array
+    :param axis: Axis to merge (0 = vertical, 1 = horizontal), integer
+    :return: merge of all images in the list, np-array
+    """
+    return np.concatenate(images, axis=axis).astype(np.uint8)
 
-    Args:
-      mask_folder_path (string) - ground truth label maps
-      nb_masks (int) - Number of masks to display
-      scale_percent (float) - Scale of image to display
+
+def overlay_image(image1, image2, ratio=0.5):
+    """
+    Overlay Two images
+    :param image1: First image
+    :param image2: Second image (must have the same size)
+    :param ratio: The factor of blending the first image
+    :return: Merged image
+    """
+
+    return (image1 * ratio + image2 * (1 - ratio)).astype("uint8")
+
+
+def merge_image_with_mask(image, mask, third_overlay=False, max_value_mask=1., axis=0):
+    """
+    Build an image with the real images and its mask
+    It's possible to add (merge) an overlay of the images
+    :param image: 3-channel image (X, X, 3), np-array
+    :param mask: 1-channel image (X, X, 1), np-array
+    :param third_overlay: Boolean to add the overlay image, bool
+    :param max_value_mask: Maximal value in the mask, float
+    :param axis: Axis to build the merge image including mask, integer
+    :return: Merged image
+    """
+
+    mask_to_display = convert_1channel_to_3channels(mask)
+    mask_to_display = convert_rgb_mask_for_displaying(mask_to_display, max_value=max_value_mask)
+
+    # Merge the image with its mask
+    merged_image = merge_images([image, mask_to_display], axis=axis)
+
+    # Add third image is third_overlay is True
+    if third_overlay:
+        overlaying_image = overlay_image(image, mask_to_display)
+        merged_image = merge_images([merged_image, overlaying_image], axis=axis)
+
+    return merged_image
+
+
+def plot_masks_from_folder(mask_folder_path, nb_masks=3, scale_percent=0.6):
+    """
+    Plot several masks (chosen randomly)
+    :param mask_folder_path: Directory containing masks, string
+    :param nb_masks: Number of masks to display, integer
+    :param scale_percent: Scale of image to display, float, [0-1]
+    :return:
     """
 
     list_masks = os.listdir(mask_folder_path)
+    nb_files = len(list_masks)
+    count = 0
 
-    for i in range(nb_masks):
-        current_mask = cv2.imread(mask_folder_path + list_masks[i])
-        current_mask = current_mask * 100
-        layers = np.concatenate((current_mask[:, :, 0], current_mask[:, :, 1], current_mask[:, :, 2]), axis=1)
+    for i in random.sample(range(nb_files), nb_masks):
+        count += 1
+        current_mask = imread(mask_folder_path + list_masks[i])
+
+        # Format
+        current_mask = convert_rgb_mask_for_displaying(current_mask, max_value=1.)
 
         # New size
-        width = int(layers.shape[1] * scale_percent)
-        height = int(layers.shape[0] * scale_percent)
+        width = int(current_mask.shape[1] * scale_percent)
+        height = int(current_mask.shape[0] * scale_percent)
         new_dim = (width, height)
 
         # Rescale
-        layers = cv2.resize(layers, new_dim)
+        current_mask = resize(current_mask, new_dim)
 
-        if i > 0:
-            images = np.concatenate((images, layers), axis=0)
+        if count > 1:
+            images = merge_images([images, current_mask], axis=1)
         else:
-            images = layers
+            images = current_mask
 
-    # Plot image
-    cv2.imshow('Several Masks', images)
-    cv2.waitKey()
+    # Plot images
+    imshow('Binary masks', images)
+    waitKey()
 
 
-def concatenate_image_with_mask(image, mask):
+def plot_images_and_masks(images: list, masks: list, third_overlay=False, scale_percent=0.6):
     """
-    Concatenate an image with its mask (and both)
-
-    Args:
-      image (cv2.Mat) - Image
-      mask (cv2.Mat) - Mask
-    """
-
-    mask_norm = postprocessing_masks_rgb(mask, image.shape)
-
-    mixed_image = cv2.addWeighted(image, 0.5, mask_norm, 0.5, 0)
-    raw_images = np.concatenate((image, mask_norm), axis=0)
-    image_and_mask = np.concatenate((raw_images, mixed_image), axis=0)
-
-    return image_and_mask
-
-
-def concatenate_several_images_with_mask(images, masks):
-    """
-    Concatenate sereval images with their mask (and both)
-
-    Args:
-      images (list of cv2.Mat) - List of Images
-      masks (list cv2.Mat) - List of Masks
+    Plot image(s) and mask(s)
+    :param images: List of images, np-array
+    :param masks: List of images, np-array
+    :param third_overlay: Boolean to add the overlay image, True/False
+    :param scale_percent: Scale of image to display, float, [0-1]
+    :return:
     """
 
-    full_images = concatenate_image_with_mask(images[0], masks[0])
+    full_image = merge_image_with_mask(images[0], masks[0], third_overlay=third_overlay, axis=0)
 
     for image, mask in zip(images[1:], masks[1:]):
-        current_example = concatenate_image_with_mask(image, mask)
-        full_images = np.concatenate((full_images, current_example), axis=1)
-
-    return full_images
-
-
-def plot_images_with_masks(images, masks, scale_percent=0.6):
-    """
-    Display an image with its mask (and both)
-
-    Args:
-      image (cv2.Mat) - Image
-      mask (cv2.Mat) - Mask
-      scale_percent (float) - Scale of image to display
-    """
-
-    if type(images) is list:
-        example = concatenate_several_images_with_mask(images, masks)
-    else:
-        example = concatenate_image_with_mask(images, masks)
+        current_image = merge_image_with_mask(image, mask, third_overlay=third_overlay, axis=0)
+        full_image = merge_images([full_image, current_image], axis=1)
 
     # New Dimension
-    height = int(example.shape[0] * scale_percent)
-    width = int(example.shape[1] * scale_percent)
+    height = int(full_image.shape[0] * scale_percent)
+    width = int(full_image.shape[1] * scale_percent)
     new_dim = (width, height)
 
-    example_resized = cv2.resize(example, new_dim)
+    full_image = resize(full_image, new_dim)
 
-    # Plot image
-    cv2.imshow('Top:Image, Middle:Mask, Bottom:Superposition', example_resized)
-    cv2.waitKey()
-
-
-def concatenate_image_mask_prediction(image, mask, prediction):
-
-    mixed_image = cv2.addWeighted(image, 0.5, prediction, 0.5, 0)
-    all_images = np.concatenate((image, mask, prediction, mixed_image), axis=0)
-
-    return all_images
+    # Plot images
+    imshow('Top:Image, Middle:Mask, Bottom:Overlay', full_image)
+    waitKey()
 
 
-def concatenate_several_images_masks_predictions(images, masks, predictions):
-    full_images = concatenate_image_mask_prediction(images[0], masks[0], predictions[0])
-
-    for image, mask, pred in zip(images[1:], masks[1:], predictions[1:]):
-        current_example = concatenate_image_mask_prediction(image, mask, pred)
-        full_images = np.concatenate((full_images, current_example), axis=1)
-
-    return full_images
-
-
-def plot_predictions(images, masks, predictions):
-
-    if type(images) is list:
-        example = concatenate_several_images_masks_predictions(images, masks, predictions)
-    else:
-        example = concatenate_image_mask_prediction(images, masks, predictions)
-
-    # Plot image
-    cv2.imshow('Top:Image, Middle:Mask, Bottom:Superposition', example)
-    cv2.waitKey()
-
-def plot_prediction(image, prediction, gt):
+def plot_predictions(images: list, predictions: list,  third_overlay=False, scale_percent=0.6):
     """
-    Display an image, the expected output and the prediction
-
-    Args:
-      image (cv2.Mat) - Image
-      prediction (cv2.Mat) - Predicted output (mask)
-      gt (cv2.Mat) - Expected output (mask)
+    Plot predictions with inputted image
+    :param images: List of images (input of the NN), np-array
+    :param predictions: List of masks (output of the NN), np-array
+    :param third_overlay: Boolean to add the overlay image, True/False
+    :param scale_percent: Scale of image to display, float, [0-1]
+    :return:
     """
 
-    image_example_gt = np.concatenate((postprocessing_images(image,
-                                                             new_size=(prediction.shape[0],
-                                                                       prediction.shape[1])),
-                                       postprocessing_masks_rgb(gt,
-                                                                new_size=(prediction.shape[0],
-                                                                          prediction.shape[1])),
-                                       postprocessing_masks_prediction(prediction,
-                                                                       new_size=(prediction.shape[0],
-                                                                                 prediction.shape[1]))),
-                                      axis=1)
-    cv2.imshow('Left:Input, center:Ground Truth, right:Prediction', image_example_gt)
-    cv2.waitKey(0)
+    full_image = merge_image_with_mask(images[0], predictions[0], third_overlay=third_overlay, axis=0)
+
+    for image, pred in zip(images[1:], predictions[1:]):
+        image_and_prediction = merge_image_with_mask(image, pred, third_overlay=third_overlay, axis=0)
+        full_image = merge_images([full_image, image_and_prediction], axis=1)
+
+    # New Dimension
+    height = int(full_image.shape[0] * scale_percent)
+    width = int(full_image.shape[1] * scale_percent)
+    new_dim = (width, height)
+
+    full_image = resize(full_image, new_dim)
+
+    imshow('Predictions', full_image)
+    waitKey()
 
 
 def plot_loss_accuracy(model_history):
