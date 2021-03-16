d1804f16e7f2925a01933d36d357d2ff78b91199
Tom <fougeretom@gmail.com>
2021-03-16 15:48:21 +0100

    Fix UT for data augmentation

--- test_data_augmentation.py
+++ test_data_augmentation.py
@@ -2,7 +2,7 @@ import cv2
 import numpy as np
 
 from data_augmentation import *
-from processing_functions import preprocessing_masks
+from processing_functions import preprocessing_masks, convert_1channel_to_3channels, convert_rgb_mask_for_displaying
 
 target_folder = "dataset/synthetic_sugarbeet_random_weeds/train_test/"
 batch_size = 10
@@ -17,18 +17,24 @@ train_image_generator, train_mask_generator = create_train_generator(target_fold
 
 def test_generators():
 
+    id = 1
+
     my_train_generator = my_image_mask_generator(train_image_generator, train_mask_generator)
 
     # Display one couple image/mask from generators
-    for (img, mask, trash) in my_train_generator:
+    for (img, mask) in my_train_generator:
+
+        assert np.max(img) <= 1.
+        assert np.max(mask) <= 1.
+
         # Image
-        cur_image = img[0] * 255
+        cur_image = img[id] * 255
         cur_image = cur_image.astype(np.uint8)
         cur_image = cv2.cvtColor(cur_image, cv2.COLOR_BGR2RGB)
 
         # Mask
-        cur_mask = np.zeros(shape=cur_image.shape)
-        cur_mask[:, :, 0] = cur_mask[:, :, 1] = cur_mask[:, :, 2] = mask[0][:, :, 0] * 255.
+        cur_mask = convert_1channel_to_3channels(mask[id])
+        cur_mask = convert_rgb_mask_for_displaying(cur_mask)
 
         # Create concatenation
         image_mask = np.concatenate((cur_image, cur_mask), axis=1)
